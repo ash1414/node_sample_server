8062a66381ec2bd88643217b7279179fd3eb009c
ash1414 <66207526+ash1414@users.noreply.github.com>
2020-06-08 15:17:21 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+// Constants
+const PORT = 6000;
+const HOST = '127.0.0.1';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(6000);
+~                   
